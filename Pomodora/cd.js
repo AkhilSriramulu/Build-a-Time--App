3329c8210af63577478d82ceed6a315c3a83eefb
AkhilSriramulu <158177823+AkhilSriramulu@users.noreply.github.com>
2025-04-20 09:25:40 +0000

    version1

---
+++ Pomodora/cd.js
@@ -0,0 +1,5 @@
+let startBtnEle = getElementById("startBtn");
+let stopBtnEle = getElementById("stopBtn");
+let resetBtnEle = getElementById("resetBtn");
+let shortBreakBtnEle = getElementById("shortBreakBtn");
+let longBreakBtnEle = getElementById("longBreakbtn");
